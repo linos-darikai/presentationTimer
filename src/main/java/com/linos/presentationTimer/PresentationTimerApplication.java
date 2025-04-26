c6da3fcdfc3f54147b349f6dd32fa1c49df7b1cb
Avalance546 <138253858+Avalance546@users.noreply.github.com>
2025-04-26 01:34:58 +0000

    Initialized the project

---
+++ src/main/java/com/linos/presentationTimer/PresentationTimerApplication.java
@@ -0,0 +1,13 @@
+package com.linos.presentationTimer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PresentationTimerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PresentationTimerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/linos/presentationTimer/PresentationTimerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.linos.presentationTimer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PresentationTimerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
